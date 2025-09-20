a35e409e44fb67a95ff4a564e68ed973871e6835
shresh <g90232770@gmail.com>
2025-09-20 12:17:28 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -34,43 +34,79 @@ app.post("/api/chat", async (req, res) => {
 
   const question = req.body.mess[1].content;
   const model = req.body.mod
+  const hist = req.body.hist
 
   try {
 
     // 1 Check for qusetion
     if (!question || !model) return res.status(200).json({ data: "Missing question" });
 
-    // 12 Check DB
-    let history = await ChatHistory.findOne({ question: question, model: model });
-    if (history) {
-      return res.status(200).json({ data: history.answer, fromCache: true });
+    if (hist){
+      // 2 Check DB
+      let history = await ChatHistory.findOne({ question: question, model: model });
+      if (history) {
+        return res.status(200).json({ data: history.answer, fromCache: true });
+      }
+      
+      // 3 If not found → call OpenRouter
+      const response = await axios.post(
+        "https://openrouter.ai/api/v1/chat/completions",
+        {
+          // model: req.body.model || 'meta-llama/llama-3.3-8b-instruct:free',
+          model: model,
+          messages: req.body.mess,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${OPENROUTER_API_KEY}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+  
+      const answer = response.data?.choices?.[0]?.message?.content || "";
+      // 4 Save to DB
+      history = new ChatHistory({
+        question: question,
+        answer: answer,
+        model: model,
+      });
+      await history.save();
+      res.status(200).json({ data: answer, fromCache: false });
+
+    }else {
+
+      // 4 If histroy is disabled
+      let history = await ChatHistory.findOne({ question: question, model: model });
+
+      const response = await axios.post(
+        "https://openrouter.ai/api/v1/chat/completions",
+        {
+          // model: req.body.model || 'meta-llama/llama-3.3-8b-instruct:free',
+          model: model,
+          messages: req.body.mess,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${OPENROUTER_API_KEY}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const answer = response.data?.choices?.[0]?.message?.content || "";
+
+      if (history) {
+      }else{
+        let alp = new ChatHistory({
+          question: question,
+          answer: answer,
+          model: model,
+        });
+        await alp.save();
+      }
+      res.status(200).json({ data: answer, fromCache: false });
     }
     
-    // 3 If not found → call OpenRouter
-    const response = await axios.post(
-      "https://openrouter.ai/api/v1/chat/completions",
-      {
-        // model: req.body.model || 'meta-llama/llama-3.3-8b-instruct:free',
-        model: model,
-        messages: req.body.mess,
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${OPENROUTER_API_KEY}`,
-          "Content-Type": "application/json",
-        },
-      }
-    );
-
-    const answer = response.data?.choices?.[0]?.message?.content || "";
-    // 4 Save to DB
-    history = new ChatHistory({
-      question: question,
-      answer: answer,
-      model: model,
-    });
-    await history.save();
-    res.status(200).json({ data: answer, fromCache: false });
   } catch (error) {
     console.error("Error in /api/chat:", error?.response?.data || error.message);
     res.status(500).json({ error: "Failed to process request" });
